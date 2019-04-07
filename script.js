f7fd8fad8f96bd1182d0703b0e34d5d08300cba3
ousmane91 <contact@ousmangueye.fr>
2019-04-08 00:44:11 +0200

    exomodif

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 //Afficher une boîte de dialogue après le focus
-
-var elem = document.getElementById('lastname');
-elem.onblur = showmessage;
-
 function showmessage() {
+  var elem = document.getElementById('lastname');
+  elem.onblur = showmessage;
   alert('merci de votre participation');
 }
